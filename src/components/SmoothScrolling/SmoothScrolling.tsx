8a638dde2dce8e2247f6dfcf089cc3b1d292d712
Sergei <sergei.borja0107@gmail.com>
2025-06-02 12:49:15 -0600

    Fixed dependencies.

--- src/components/SmoothScrolling/SmoothScrolling.tsx
+++ src/components/SmoothScrolling/SmoothScrolling.tsx
@@ -1,18 +1,13 @@
 "use client";
 import type { LenisOptions } from "@studio-freight/lenis";
 import { ReactLenis } from "@studio-freight/react-lenis";
-import { ReactNode } from "react";
-
-interface SmoothScrollingProps {
-    children?: ReactNode;
-}
 
 const lenisOptions: LenisOptions = {
     duration: 1.5,
     lerp    : 0.1
 };
 
-function SmoothScrolling({children}: SmoothScrollingProps) {
+function SmoothScrolling() {
     return (
         <ReactLenis root options={lenisOptions}/>
     );
